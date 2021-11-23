5ff30103313d0369f09ed7b78672f809944b0ce1
Nathalie Lavroff <nathalielavroff@gmail.com>
2021-11-23 10:26:41 +0100

    navbar added

--- header.php
+++ header.php
@@ -1,5 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">DonkeyEvent</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Cinéma</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Danse</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Théâtre</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Concert</a>
+    </ul>
+    <div class="col-md-8 bg-light text-right">
+        <a href="login.php">
+        <input class="btn login" type="submit" value="Se connecter">
+        </a>    
+    </div>
+    </div>
+  
+</nav>
+
 <?php
 
 require_once 'research.php';
 
 ?>
+
