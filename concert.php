93306176372b58cc7d100617a71111aa8e01403c
Nathalie Lavroff <nathalielavroff@gmail.com>
2021-11-25 22:35:02 +0100

    Progress on concert page

--- concert.php
+++ concert.php
@@ -3,11 +3,25 @@
 require_once 'connec.php';
 require_once 'index.html';
 require_once 'header.php';
+?>
 
-date_default_timezone_set('Europe/Paris');
-$currentDate = date("Y-m-d H:i:s");
+<section class="bgimage-concert">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="text-center">
+                <h1>CONCERTS</h1>
+            </div>
+        </div>
+    </div>
+</section>
 
-$query = "SELECT cover, Event.name as event, Event.description as description, Artist.name as artist 
+<div class="space"><div>
+
+        <?php
+        date_default_timezone_set('Europe/Paris');
+        $currentDate = date("Y-m-d H:i:s");
+
+        $query = "SELECT cover, Event.name as event, Event.description as description, Artist.name as artist 
 FROM Event
     JOIN Session ON idEvent = Event_idEvent
     JOIN Performance ON Performance.Event_idEvent = Event.idEvent
@@ -15,29 +29,35 @@ FROM Event
 WHERE DATE(Session.date) >= '$currentDate' AND Genre_idGenre = 4
 ";
 
-$statement = $pdo->prepare($query);
-$statement->execute();
-$concerts = $statement->fetchAll(PDO::FETCH_ASSOC);
-?>
+        $statement = $pdo->prepare($query);
+        $statement->execute();
+        $concerts = $statement->fetchAll(PDO::FETCH_ASSOC);
+        ?>
 
-
-<?php $concerts = array_map("unserialize", array_unique(array_map("serialize", $concerts))); ?>
-<?php foreach ($concerts as $concert) { ?>
-    <div class="container-fluid">
-        <div class="row row-cols-2">
-            <div class="col">
-                <img src="Cover/<?php echo $concert['cover']; ?>" class="col-md-8 float-md-end mb-3 ms-md-3">
-            </div>
-            <div class="col">
-                <h1><?php echo $concert['event']; ?></h1>
-                <div class="row">
+        <?php $concerts = array_map("unserialize", array_unique(array_map("serialize", $concerts))); ?>
+        <?php foreach ($concerts as $concert) { ?>
+            <div class="container-fluid">
+                <div class="row row-cols-2">
                     <div class="col">
-                        <?php echo $concert['description']; ?>
+                        <img src="Cover/<?php echo $concert['cover']; ?>" class="img-fluid">
+                    </div>
+                    <div class="col">
+                        <h1><?php echo $concert['event']; ?></h1>
+                        <div class="row">
+                            <div class="col">
+                                <?php echo $concert['description']; ?>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <form method="GET" action="product.php" name="product">
+                                <input type="hidden" name="idEvent" value="<?php echo $concert["idEvent"];?>" />
+                                <button type="submit">En savoir plus</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-<?php }
-require_once 'footer.php'
-?>
+            <div class="space"><div>
+        <?php }
+        require_once 'footer.php'
+        ?>
