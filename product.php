065ed1cfe331c96686cf5d9d5558f239dc462b0c
Nathalie Lavroff <nathalielavroff@gmail.com>
2021-11-26 18:21:18 +0100

    added red alert on passed events

--- product.php
+++ product.php
@@ -74,7 +74,7 @@ $sessions = $statement->fetchAll();
                 <td><?php echo $session['venue']; ?></td>
                 <td><?php echo $session['price']; ?>€</td>
                 <?php if ($currentDate >= $session['date']) { ?>
-                    <td>Cet événement est passé</td>
+                    <td><span class="alert-message">Cet événement est passé</span></td>
                 <?php } else { ?>
                     <td>
                         <form method="post" action="cart.php" name="cart">
