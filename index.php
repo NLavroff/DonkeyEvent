fb0d26c0769f1f08e4ef07913c7bd84c1404f04f
Jon2may <jonathan.demay004@gmail.com>
2021-11-26 10:29:29 +0100

    added style + hover on index page text

--- index.php
+++ index.php
@@ -12,14 +12,12 @@ $statement = $pdo->query($query);
 $events = $statement->fetchAll(); 
 ?>
 
+<!-- video hero banner -->
 <header>
-
-  <!-- The HTML5 video element that will create the background video on the header -->
   <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
     <source src="medias/homepage_video.mp4" type="video/mp4">
   </video>
 
-  <!-- The header content -->
   <div class="container h-100">
     <div class="d-flex h-100 text-center align-items-center">
       <div class="w-100 text-white">
@@ -29,56 +27,65 @@ $events = $statement->fetchAll();
   </div>
 </header>
 
-<div class="container">
-    <div class="text-center">
-        <p>È universalmente riconosciuto che un lettore che osserva il layout di una pagina viene distratto dal contenuto testuale se questo è leggibile. Lo scopo dell’utilizzo del Lorem Ipsum è che offre una normale distribuzione delle lettere (al contrario di quanto avviene se si utilizzano brevi frasi ripetute, ad esempio “testo qui”), apparendo come un normale blocco di testo leggibile. Molti software di impaginazione e di web design utilizzano Lorem Ipsum come testo modello. Molte versioni del testo sono state prodotte negli anni, a volte casualmente, a volte di proposito (ad esempio inserendo passaggi ironici).</p>     
-    </div>
-</div>
-
-<div class="container">
-
-<div class="container img-genre text-center ">
-        <img src="medias/cinema.jpg" class="w-60 img-fluid">
-        <h3 class="genre">CINÉMA</h3>
-</div>
-
-<div class="container img-genre text-center ">
-        <img src="medias/expo.jpg" class="w-60 img-fluid">
-        <h3 class="genre">EXPOSITIONS</h3>
-</div>
-
-<div class="container img-genre text-center ">
-        <img src="medias/concert.jpg" class="w-60 img-fluid">
-        <h3 class="genre">CONCERTS</h3>
-</div>
-
-<div class="container img-genre text-center ">
-        <img src="medias/danse.jpg" class="w-60 img-fluid">
-        <h3 class="genre">DANSE</h3>
-</div>
-
-<div class="container img-genre text-center ">
-        <img src="medias/theatre.jpg" class="w-60 img-fluid">
-        <h3 class="genre">THÉÂTRE</h3>
-</div>
-        
-</div>
+<section class="genre-section">
+      <div class="container">
+          <div class="text-center">
+              <p>È universalmente riconosciuto che un lettore che osserva il layout di una pagina viene distratto dal contenuto testuale se questo è leggibile. Lo scopo dell’utilizzo del Lorem Ipsum è che offre una normale distribuzione delle lettere (al contrario di quanto avviene se si utilizzano brevi frasi ripetute, ad esempio “testo qui”), apparendo come un normale blocco di testo leggibile. Molti software di impaginazione e di web design utilizzano Lorem Ipsum come testo modello. Molte versioni del testo sono state prodotte negli anni, a volte casualmente, a volte di proposito (ad esempio inserendo passaggi ironici).</p>     
+          </div>
+      </div>
 
 
-<ul>
-    <?php foreach($events as $event): ?>
-        <form method="GET" action="product.php" name="product">
-            <input type="hidden" name="idEvent" value="<?php echo $event["idEvent"]; ?>" />
-            <li><?php echo $event['name'] . BR . $event['description']; ?></li>
-            <button type="submit">En savoir plus</button>
-        </form>
-    <?php endforeach ?>
-</ul>
+      <!-- section genre -->
+      <div class="container">
+          <div class="container img-genre text-center">
+                  <a href="cinema.php">
+                  <img src="medias/cinema.jpg" alt="cinema" class="w-60 img-fluid">
+                  <h2 class="genre">CINÉMA</h2>
+                  </a>
+          </div>
+
+          <div class="container img-genre text-center">
+                  <a href="exhibit.php">
+                  <img src="medias/expo.jpg" alt="exposition" class="w-60 img-fluid">
+                  <h2 class="genre">EXPOSITIONS</h2>
+                  </a>
+          </div>
+
+          <div class="container img-genre text-center">
+                  <a href="concert.php">
+                  <img src="medias/concert.jpg" alt="concert" class="w-60 img-fluid">
+                  <h2 class="genre">CONCERTS</h2>
+                  </a>
+          </div>
+
+          <div class="container img-genre text-center">
+                  <a href="dance.php">
+                  <img src="medias/danse.jpg" alt="danse" class="w-60 img-fluid">
+                  <h2 class="genre">DANSE</h2>
+                  </a>
+          </div>
+
+          <div class="container img-genre text-center">
+                  <a href="theater.php">
+                  <img src="medias/theatre.jpg" alt="theatre" class="w-60 img-fluid">
+                  <h2 class="genre">THÉÂTRE</h2>
+                  </a>
+          </div>        
+      </div>
 
 
+      <ul>
+          <?php foreach($events as $event): ?>
+              <form method="GET" action="product.php" name="product">
+                  <input type="hidden" name="idEvent" value="<?php echo $event["idEvent"]; ?>" />
+                  <li><?php echo $event['name'] . BR . $event['description']; ?></li>
+                  <button type="submit">En savoir plus</button>
+              </form>
+          <?php endforeach ?>
+      </ul>
+</section>
 
 
 <?php
 require_once 'footer.php';
 ?>
-
