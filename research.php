a8b9463246288482c5761d483acfde04a20c1214
Nathalie Lavroff <nathalielavroff@gmail.com>
2021-11-27 16:03:06 +0100

    added style to research feature

--- research.php
+++ research.php
@@ -3,40 +3,35 @@
 require_once 'connec.php';
 
 ?>
-<!-- A bit of style, to be removed when we develop the CSS part of the project -->
-<style>
-table {
-  font-family: arial, sans-serif;
-  color: #F1DAC4;
-  border-collapse: collapse;
-  width: 100%;
-}
 
-td, th {
-  border: 1px solid #474973;
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even) {
-  background-color: #dddddd;
-  color: #161B33;
-}
-</style>
-
-<!-- A simple form to know what the user is looking for -->
-<h1>Recherche</h1>
-<form action="searchResult.php" method="get" autocomplete="on">
-    <label for="search">Je souhaite réserver : </label>
-    <input type="text" name="search" placeholder="Artiste/Salle/Ville" value="<?php if (isset($_GET["search"])) { echo $_GET["search"]; } ?>">
-    <label for="searchDate">le : </label>
-    <input type="date" name="searchDate" value=<?php if (isset($_GET["searchDate"])) { echo $_GET["searchDate"]; } ?>>
-    <label for="searchPrice">Prix maximum par place : </label>
-    <select name="searchPrice">
-      <option value=100>100 €</option>
-      <option value=50>50 €</option>
-      <option value=20>20 €</option>
-      <option value=5>5 €</option>
-    </select>
-    <input type="submit" value="Rechercher"><br><br>
-</form>
+<div class="container-fluid research">
+  <div class="row">
+    <div class="col-5">
+      <h1>Recherche</h1>
+    </div>
+    <form class="searchbar" action="searchResult.php" method="get" autocomplete="on">
+      <div class="col-5">
+        <label for="search">Je souhaite réserver : </label>
+        <input class="form-control form-control-sm" type="text" name="search" placeholder="Artiste/Salle/Ville" value="<?php if (isset($_GET["search"])) {
+                                                                                                                          echo $_GET["search"];
+                                                                                                                        } ?>">
+      </div>
+      <div class="col-5">
+        <label for="searchDate">le : </label>
+        <input class="form-control form-control-sm" type="date" name="searchDate" value=<?php if (isset($_GET["searchDate"])) {
+                                                                                          echo $_GET["searchDate"];
+                                                                                        } ?>>
+      </div>
+      <div class="col-5">
+        <label for="searchPrice">Prix maximum par place : </label>
+        <select name="searchPrice" class="form-select">
+          <option value=100>100 €</option>
+          <option value=50>50 €</option>
+          <option value=20>20 €</option>
+          <option value=5>5 €</option>
+        </select>
+        <input class="btn btn-light" type="submit" value="Rechercher"><br><br>
+    </form>
+  </div>
+</div>
+</div>
